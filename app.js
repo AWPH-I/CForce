e835bb5a6e6c5a38de534b9777cf8e8fe49efdd8
aWph-- <admin@moosebot.me>
2017-12-10 14:46:46 +0000

    Let's try this

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ global.io = require('socket.io')(server);
 server.listen(8081);
 
 // connect to db
-mongoose.connect('mongodb://127.0.0.1:27017/CForce');
+mongoose.createConnection('mongodb://localhost:27017/CForce');
 const db = mongoose.connection;
 
 //sort out mongoose models
