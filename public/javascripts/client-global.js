be6fcd6314c0c50901838657dd00a71a4e2259e0
aWph-- <admin@moosebot.me>
2017-12-14 17:44:24 +0000

    Fixed

--- public/javascripts/client-global.js
+++ public/javascripts/client-global.js
@@ -13,31 +13,27 @@ $.fn.extend({
     }
 });
 
-function error(data) {
-    const wrapper = document.createElement('div');
-    wrapper.className = 'alert-wrapper';
-    $('#main-container').append(wrapper);
-    
+function error(data) { 
     const err = document.createElement('div');
     err.className = 'animated bounceIn alert-card alert alert-' + data.type;
     err.innerHTML = '<b>' + data.title + '</b><br>' + data.body;
     
     const close = document.createElement('i');
     close.onclick= function() { 
-        var cache = $(this.parentNode.parentNode);
-        $(this.parentNode).animateCss('bounceOut', function() {
+        var cache = $(this.parentNode);
+        cache.animateCss('bounceOut', function() {
             cache.remove(); 
         });
     };
     close.className = 'fa fa-times-circle alert-close';
 
     $(err).append(close);
-    $(wrapper).append(err);
+    $('body').append(err);
 }
 
 socket.on('error-receive', function (data) {
-    //data.title: short bold title
-    //data.body: main msg
-    //data.type: warning, success, danger, dark etc
     error(data);
+    if($('.alert-card').length > 3) {
+        $('.alert-card')[0].remove();
+    }
 });
