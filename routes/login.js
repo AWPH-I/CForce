975916115e1ecbfa35d7347cfeb1f44a2aec5ff1
aWph-- <admin@moosebot.me>
2017-12-14 18:22:31 +0000

    t

--- routes/login.js
+++ routes/login.js
@@ -4,8 +4,10 @@ var User = require('mongoose').model('user');
 
 router.get('/', function(req, res, next) {
     res.render('login', {title: 'Login', isLoggedIn: req.isLoggedIn});
-    io.to(req.session.sock).emit('error-receive', {title: 'Empty fields!', body:'Please fill in all required information to login.', type:'danger'});
-    console.log('x');
+    setTimeOut(3000, () => {
+        io.to(req.session.sock).emit('error-receive', {title: 'Empty fields!', body:'Please fill in all required information to login.', type:'danger'});
+        console.log('x');
+    });
 });
 
 router.post('/', function (req, res, next) {
