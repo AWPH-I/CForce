033bc09e183f960a3ae24b83dd6019cd4e530fd8
aWph-- <admin@moosebot.me>
2017-12-15 17:34:08 +0000

    Test

--- routes/login.js
+++ routes/login.js
@@ -13,6 +13,7 @@ router.post('/', function (req, res, next) {
                 res.json({ err:{title: 'Invalid credentials!', body:'The details you have provided are invalid.', type:'danger' } });
             } else {
                 req.session.userId = user._id;
+                console.log('redir');
                 res.redirect('/profile');
             }
         });
