66abc87a6fc2d4ed612d40e3cc0c1934200e67f5
aWph-- <admin@moosebot.me>
2017-12-24 13:24:28 +0000

    v0.0.7(.1) - Added hacktimer.js to fix tabbed-out timer issue
    Not a real version, just a quick little fix I wanted to push

--- public/javascripts/client-global.js
+++ public/javascripts/client-global.js
@@ -40,6 +40,11 @@ function throwErr(data) {
     }
 }
 
+const BuildWorker = function(foo){
+   var str = foo.toString().match(/^\s*function\s*\(\s*\)\s*\{(([\s\S](?!\}$))*[\s\S])/)[1];
+   return new Worker(window.URL.createObjectURL(new Blob([str],{type:'text/javascript'})));
+}
+
 socket.on('update-ui-res', function(data) {
     console.log(data);
     if(data.balance != null) $('#balance-text').text(data.balance);
